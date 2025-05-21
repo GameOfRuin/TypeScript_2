a25a34d99d8228f98c1583112fc49a68c51e92be
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-21 17:51:15 +0300

    reduce. Баланс

--- src/main.ts
+++ src/main.ts
@@ -678,12 +678,73 @@
 // ]
 //  */
 
-const deduplicate = (word: string) => {
-  const massiveWord = word.split(''); // Создаем массив из слова
-  return massiveWord.reduce((acc: string, cur: string) => {
-    return acc.includes(cur) ? acc : acc + cur;
-  }, '');
+// const deduplicate = (word: string) => {
+//   const massiveWord = word.split(''); // Создаем массив из слова
+//   return massiveWord.reduce((acc: string, cur: string) => {
+//     return acc.includes(cur) ? acc : acc + cur;
+//   }, '');
+// };
+//
+// const result = deduplicate('uuunbbeliaaaaveeabbbblllllee');
+// console.log(result); // unbeliaveable
+
+/*
+Вам даны 2 пользователя банковской системы.
+У каждого пользователя есть список проведенных операций по банковской карте.
+Каждая операция может быть как положительной (пополнение), так и отрицательной (снятие).
+
+При открытии счета у пользователя есть стартовый баланс - он может быть как отрицательным, так и положительным.
+
+Напишите функцию calculateBalanceForUser(user: User): number, которая принимает на вход объект типа User,
+а возвращает число - текущий баланс пользователя.
+С помощью reduce просто просуммируйте стартовый баланс и все операции.
+ */
+
+type Transaction = {
+  id: number;
+  diff: number;
 };
 
-const result = deduplicate('uuunbbeliaaaaveeabbbblllllee');
-console.log(result); // unbeliaveable
+type User = {
+  id: number;
+  name: string;
+  startBalance: number;
+  transactions: Transaction[];
+};
+
+const alexander: User = {
+  id: 37,
+  name: 'AleX',
+  startBalance: 0,
+  transactions: [
+    { id: 8, diff: 100 },
+    { id: 30, diff: -50 },
+    { id: 23, diff: 250 },
+  ],
+};
+
+const max: User = {
+  id: 40,
+  name: 'MaX',
+  startBalance: -3,
+  transactions: [
+    { id: 4, diff: 9 },
+    { id: 5, diff: -18 },
+    { id: 5, diff: 3 },
+  ],
+};
+
+const calculateBalanceForUser = (user: User): number => {
+  const transaction = user.transactions;
+  return (
+    transaction.reduce((acc: number, cur: Transaction) => {
+      return acc + cur.diff;
+    }, 0) + user.startBalance
+  );
+};
+
+const alexanderBalance = calculateBalanceForUser(alexander); // Его история операция: 0 + 100 - 50 + 250
+console.log(alexanderBalance); // 300
+
+const maxBalance = calculateBalanceForUser(max); // Его история операция: -3 + 9 - 18 + 3
+console.log(maxBalance); // -9
