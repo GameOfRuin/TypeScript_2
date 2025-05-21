ce9b302a50fb0fc90ef7172fe2d019b05acf4f03
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-21 21:26:54 +0300

    reduce. Оценки

--- src/main.ts
+++ src/main.ts
@@ -700,51 +700,91 @@
 С помощью reduce просто просуммируйте стартовый баланс и все операции.
  */
 
-type Transaction = {
-  id: number;
-  diff: number;
-};
+// type Transaction = {
+//   id: number;
+//   diff: number;
+// };
+//
+// type User = {
+//   id: number;
+//   name: string;
+//   startBalance: number;
+//   transactions: Transaction[];
+// };
+//
+// const alexander: User = {
+//   id: 37,
+//   name: 'AleX',
+//   startBalance: 0,
+//   transactions: [
+//     { id: 8, diff: 100 },
+//     { id: 30, diff: -50 },
+//     { id: 23, diff: 250 },
+//   ],
+// };
+//
+// const max: User = {
+//   id: 40,
+//   name: 'MaX',
+//   startBalance: -3,
+//   transactions: [
+//     { id: 4, diff: 9 },
+//     { id: 5, diff: -18 },
+//     { id: 5, diff: 3 },
+//   ],
+// };
+//
+// const calculateBalanceForUser = (user: User): number => {
+//   const transaction = user.transactions;
+//   return (
+//     transaction.reduce((acc: number, cur: Transaction) => {
+//       return acc + cur.diff;
+//     }, 0) + user.startBalance
+//   );
+// };
+//
+// const alexanderBalance = calculateBalanceForUser(alexander); // Его история операция: 0 + 100 - 50 + 250
+// console.log(alexanderBalance); // 300
+//
+// const maxBalance = calculateBalanceForUser(max); // Его история операция: -3 + 9 - 18 + 3
+// console.log(maxBalance); // -9
 
-type User = {
-  id: number;
-  name: string;
-  startBalance: number;
-  transactions: Transaction[];
-};
+const values = [
+  [1, 100],
+  [2, 200],
+  [3, 300],
+  [1, 2],
+  [3, 400],
+  [4, 500],
+  [1, 99],
+];
 
-const alexander: User = {
-  id: 37,
-  name: 'AleX',
-  startBalance: 0,
-  transactions: [
-    { id: 8, diff: 100 },
-    { id: 30, diff: -50 },
-    { id: 23, diff: 250 },
-  ],
-};
+const result = values.reduce(
+  (acc: Record<string, { count: number; sum: number }>, cur: number[]) => {
+    const evaluation = cur[0].toString();
+    if (!acc[evaluation]) {
+      acc[evaluation] = { count: 1, sum: cur[1] };
+      return acc;
+    }
+    acc[evaluation].count += 1;
+    acc[evaluation].sum += cur[1];
+    return acc;
+  },
+  {} as Record<string, { count: number; sum: number }>,
+);
 
-const max: User = {
-  id: 40,
-  name: 'MaX',
-  startBalance: -3,
-  transactions: [
-    { id: 4, diff: 9 },
-    { id: 5, diff: -18 },
-    { id: 5, diff: 3 },
-  ],
-};
+console.log(result);
+/*
+{
+  '1': { count: 3, sum: 201 },
+  '2': { count: 1, sum: 200 },
+  '3': { count: 2, sum: 700 },
+  '4': { count: 1, sum: 500 }
+}
 
-const calculateBalanceForUser = (user: User): number => {
-  const transaction = user.transactions;
-  return (
-    transaction.reduce((acc: number, cur: Transaction) => {
-      return acc + cur.diff;
-    }, 0) + user.startBalance
-  );
-};
-
-const alexanderBalance = calculateBalanceForUser(alexander); // Его история операция: 0 + 100 - 50 + 250
-console.log(alexanderBalance); // 300
-
-const maxBalance = calculateBalanceForUser(max); // Его история операция: -3 + 9 - 18 + 3
-console.log(maxBalance); // -9
+(Объяснение) Если не поняли, это означает, что:
+Оценка 1 встретилась 3 раза - [1, 100], [1, 2] и [1, 99] и всего = 201 чел (100+2+99)
+Оценка 2 встретилась 1 раз - [2, 200] и всего 200 чел
+Оценка 3 встретилась 2 раза - [3, 300] и [3, 400] и всего 700 чел
+Оценка 4 встретилась 1 раз - [4, 500] и всего 500 чел
+ */
