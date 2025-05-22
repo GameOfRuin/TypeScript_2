9573e8a4da145f74ad65e422747bc5e7b8021058
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-22 21:43:47 +0300

    Расширение

--- src/main.ts
+++ src/main.ts
@@ -858,49 +858,86 @@
 // console.log(nos); // [ 'no-1', 'no-2' ]
 // console.log(cos); // [ 'zzz' ]
 // console.log(b1); // { first: 'castfore', child: null }
-const megaobject = {
-  child: null,
-  child1: {
-    nick: 'name',
-    nicknames: ['n1', 'n2', { n: 'n', b: 'b' }],
-    james: {
-      kates: {
-        nicks: {
-          nicknames: ['nn', 'vv', 'gg'],
-          child: {
-            nick: 'childName',
-            child: undefined,
-          },
-        },
-      },
-    },
-  },
-  child2: {},
-  child3: 'child3',
+// const megaobject = {
+//   child: null,
+//   child1: {
+//     nick: 'name',
+//     nicknames: ['n1', 'n2', { n: 'n', b: 'b' }],
+//     james: {
+//       kates: {
+//         nicks: {
+//           nicknames: ['nn', 'vv', 'gg'],
+//           child: {
+//             nick: 'childName',
+//             child: undefined,
+//           },
+//         },
+//       },
+//     },
+//   },
+//   child2: {},
+//   child3: 'child3',
+// };
+//
+// const {
+//   child,
+//   child1: {
+//     nick,
+//     nicknames,
+//     james: {
+//       kates: {
+//         nicks: {
+//           nicknames: [nicknames1, ...otherNicks],
+//           child: { nick: nick1, child: child1 },
+//         },
+//       },
+//     },
+//   },
+//   ...otherChilds
+// } = megaobject;
+//
+// console.log(child); // null
+// console.log(otherChilds); // { child2: {}, child3: 'child3' }
+// console.log(nick); // name
+// console.log(nicknames); // [ 'n1', 'n2', { n: 'n', b: 'b' } ]
+// console.log(nicknames1); // nn
+// console.log(otherNicks); // [ 'vv', 'gg' ]
+// console.log(nick1); // childName
+// console.log(child1); // undeined
+// type Person = {
+//   name: string;
+// };
+//
+// type Paginated<T = never> = {
+//   // Вопрос про невер
+//   total: number;
+//   limit: number;
+//   offset: number;
+//   items: T[];
+// };
+//
+// const responseOfPersons: Paginated<Person> = {
+//   total: 10,
+//   limit: 5,
+//   offset: 0,
+//   items: [{ name: 'P1' }],
+// };
+//
+// const responseOfSomething: Paginated = {
+//   total: 10,
+//   limit: 5,
+//   offset: 0,
+//   items: [],
+// };
+
+type Person = {
+  name: string;
+};
+type WithId<T> = T & {
+  id: number;
 };
 
-const {
-  child,
-  child1: {
-    nick,
-    nicknames,
-    james: {
-      kates: {
-        nicks: {
-          nicknames: [nicknames1, ...otherNicks],
-          child: { nick: nick1, child: child1 },
-        },
-      },
-    },
-  },
-  ...otherChilds
-} = megaobject;
-
-console.log(child); // null
-console.log(otherChilds); // { child2: {}, child3: 'child3' }
-console.log(nick); // name
-console.log(nicknames); // [ 'n1', 'n2', { n: 'n', b: 'b' } ]
-console.log(nicknames1); // nn
-console.log(otherNicks); // [ 'vv', 'gg' ]
-console.log(nick1); // childName
-console.log(child1); // undeined
+const p: WithId<Person> = {
+  id: 1,
+  name: 'name',
+};
