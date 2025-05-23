4a325513840f65bbe290766860fd660c8d293aef
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-23 17:35:36 +0300

    Flat массив известный функция

--- src/main.ts
+++ src/main.ts
@@ -930,6 +930,7 @@
 //   offset: 0,
 //   items: [],
 // };
+
 const matrix = [
   [1, 2, 4],
   [3, 5, 4],
@@ -937,13 +938,28 @@ const matrix = [
   [3, 5, 9],
 ];
 
-const result = matrix.reduce((acc: number[], cur: number[]): number[] => {
-  for (let i = 0; i < cur.length; i++) {
-    if (!acc.includes(cur[i])) {
-      acc.push(cur[i]);
+// const result = matrix.reduce((acc: number[], cur: number[]): number[] => {
+//   for (let i = 0; i < cur.length; i++) {
+//     if (!acc.includes(cur[i])) {
+//       acc.push(cur[i]);
+//     }
+//   }
+//   return acc;
+// }, []);
+//
+// console.log(result.sort((a: number, b: number) => a - b));
+
+const flatUniq = (matrix: number[][]) => {
+  const result: number[] = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const massive = matrix[i];
+    for (let j = 0; j < massive.length; j++) {
+      if (!result.includes(massive[j])) {
+        result.push(massive[j]);
+      }
     }
   }
-  return acc;
-}, []);
+  return result.sort((a: number, b: number) => a - b);
+};
 
-console.log(result.sort((a: number, b: number) => a - b));
+console.log(flatUniq(matrix));
